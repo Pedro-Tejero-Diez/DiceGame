4fd4b3cadd675968697dfb3783fbe5adac207830
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-11-28 06:34:02 +0100

    update home 28 nov 2023

--- init-db.js
+++ init-db.js
@@ -1,4 +1,19 @@
-// init-db.js
+var user = "user";
+var password = "password";
 
-// Create a sample database and collection
-db.getSiblingDB('Dice_Game').createCollection('gamers');
+// Connect to the admin database to create the user
+db = db.getSiblingDB('admin');
+
+// Check if the user already exists
+var user = db.getUsers({ user: "user" });
+
+if (!user.length) {
+  // User doesn't exist, create it
+  db.createUser({
+    user: "user",
+    pwd: "password",
+    roles: [
+      { role: "readWrite", db: "Dice_Game" },
+
+    ],
+  });
