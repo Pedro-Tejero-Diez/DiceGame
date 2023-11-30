0d4e43cefbf5be3eb35f7cbf6075b1a8be6c09ce
pedrete3000 <pedrete3000@hotmail.com>
2023-11-30 15:37:38 +0100

    update jobplace 30 nov 2023

--- init-db.js
+++ init-db.js
@@ -1,19 +1,6 @@
-var user = "user";
-var password = "password";
+//run shell
+monogosh
+//enter database
+db.auth("user", "password");
 
-// Connect to the admin database to create the user
-db = db.getSiblingDB('admin');
-
-// Check if the user already exists
-var user = db.getUsers({ user: "user" });
-
-if (!user.length) {
-  // User doesn't exist, create it
-  db.createUser({
-    user: "user",
-    pwd: "password",
-    roles: [
-      { role: "readWrite", db: "Dice_Game" },
-
-    ],
-  });
+use Dice_Game;
